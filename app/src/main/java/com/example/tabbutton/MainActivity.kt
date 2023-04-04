7ba1442cc4786d40ccec65cfa9f60e3a6f162c34
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-04-04 12:13:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tabbutton/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.tabbutton
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tabbutton.ui.theme.TabbuttonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TabbuttonTheme {
+               MyApp()
+
+            }
+        }
+    }
+}
+@Composable
+fun MyApp(){
+    var moneyCounter = remember {
+        mutableStateOf(0)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(text = "$${moneyCounter.value}", style = TextStyle(
+                color = Color.White,
+                fontSize = 39.sp,
+                fontWeight = FontWeight.ExtraBold
+            ))
+            Spacer(modifier = Modifier.height(130.dp))
+            CreateCircle(moneyCounter = moneyCounter.value ){
+                moneyCounter.value = it + 1
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun CreateCircle(moneyCounter : Int = 0 , updateMoneyCounter : (Int) -> Unit){
+
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(45.dp)
+        .clickable {
+          updateMoneyCounter(moneyCounter)
+        },shape = CircleShape, elevation = 4.dp) {
+        Box(contentAlignment = Alignment.Center){
+            Text(text = "tap $moneyCounter", modifier = Modifier)
+
+        }
+
+    }
+}
+ 
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TabbuttonTheme {
+        MyApp()
+    }
+}
